9624bb76a4e7eee5e7a8fc766adcc9dd0df2dd32
tsuigrac <tsuigrac@grinnell.edu>
2019-01-28 09:39:03 -0600

    Hello class added

---
+++ src/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package hello;
+
+import java.io.PrintWriter;
+
+public class HelloWorld {
+  public static void main (String[] args) {
+    PrintWriter pen = new PrintWriter(System.out, true);
+    pen.println ("Hello, GitHub!");
+    pen.flush();
+  } // main(String[])
+}
